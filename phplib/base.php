f14bc3f9c13f9fb64d3c3318c0fcd3f222229428
Kevin M Granger <kgranger@box.com>
2015-07-06 17:58:09 -0700

    Make base.php require style consistent

--- phplib/base.php
+++ phplib/base.php
@@ -10,7 +10,7 @@ require_once 'oncall.php';
 require_once 'irccat.php';
 require_once 'pagination.class.php';
 // report.php contains report rendering functions
-include_once("phplib/report.php");
+include_once 'phplib/report.php';
 
 $pages = array("/index.php" => "Overview", "/add.php" => "Add", "/report.php" => "Reports", "/meeting.php" => "Meeting");
 $pages_icon = array("/index.php" => "icon-home", "/add.php" => "icon-plus-sign", "/report.php" => "icon-list-alt", "/meeting.php" => "icon-bullhorn");
